1765c3beee7419b0a1a4f89d5cc98f25a4bc3284
Dayana <dayana.nashkova@yahoo.com>
2023-06-19 09:36:33 +0000

    feat: add messaging option

--- codestar/settings.py
+++ codestar/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'django_summernote',
     'blog',
 ]
@@ -55,6 +56,9 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
